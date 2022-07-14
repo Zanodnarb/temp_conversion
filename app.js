b74947492f29181d3ffaccdd6b80b652ee9acd51
zanodnarb <103510789+Zanodnarb@users.noreply.github.com>
2022-07-14 16:18:08 -0400

    create app.js
    created file and input the conversions

---
+++ app.js
@@ -0,0 +1,8 @@
+const kelvin = 293 // kelvin will stay constant at 293
+const celsius = kelvin - 273 // celsius = kelvin tempt - 273
+let fahrenheit = celsius * (9/5) + 32
+let newton = celsius * (33/100)
+newton = Math.floor(newton)
+fahrenheit = Math.floor(fahrenheit); // returns the largest integer <= 8
+console.log('The temperature is ' + fahrenheit + ' degrees Fahrenheit'); 
+console.log('The temperature is ' + newton + ' degrees Newton')
